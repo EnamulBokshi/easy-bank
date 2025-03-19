2230ac873950458f2906b4c48e5b9766c3a5610e
enamulbokshi <enamulbokshi@gmail.com>
2025-03-19 17:46:38 +0600

    initial configurations and setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function RootLayout({children}:Readonly<{children:React.ReactNode}>) {
+  return (
+   <main>
+    {
+        children
+    }
+   </main>
+  )
+}
+
+export default RootLayout
